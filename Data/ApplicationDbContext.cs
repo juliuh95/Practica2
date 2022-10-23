e109abbfe5dfa82f15bf95e38622eb3cdc46685e
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2022-10-23 15:03:50 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
