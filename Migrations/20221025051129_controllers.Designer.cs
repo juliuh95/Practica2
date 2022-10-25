2329eda9fd488bb311ad3fd551463f02fc1b131d
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2022-10-25 00:13:04 -0500

    migrations

--- Migrations/20221025051129_controllers.Designer.cs
+++ Migrations/20221025051129_controllers.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practica2.Data;
 
@@ -10,9 +11,10 @@ using Practica2.Data;
 namespace Practica2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221025051129_controllers")]
+    partial class controllers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
